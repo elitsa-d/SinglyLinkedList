d2efe89421d4156f23e238e7b21daa650f36a011
Elitsa Dimitrova <elitsa.v.dimitrova@protonmail.com>
2023-09-21 19:50:28 +0300

    Create static method for adding two numbers represented by lists

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -43,5 +43,56 @@ public class Main {
 
         list.removeAll();
         System.out.println("Size of list after calling 'removeAll()' method: " + list.getSize());
+
+        SinglyLinkedList list1 = new SinglyLinkedList();
+        list1.add(7);
+        list1.add(1);
+        list1.add(6);
+
+        SinglyLinkedList list2 = new SinglyLinkedList();
+        list2.add(5);
+        list2.add(9);
+        list2.add(2);
+
+        SinglyLinkedList sum = sumLists(list1, list2);
+        sum.print();
+    }
+
+    /**
+     * Sums two numbers represented in a linked list. Every node contains a single digit and the digits are stored in reverse order.
+     *
+     * @param list1   the first number represented in a linked list
+     * @param list2   the second number represented in a linked list
+     *
+     * @return a new linked list representing the result where digits are again in reverse
+     */
+
+    public static SinglyLinkedList sumLists(SinglyLinkedList list1, SinglyLinkedList list2) {
+        SinglyLinkedList result = new SinglyLinkedList();
+        int addend1 = 0;
+        int addend2 = 0;
+        int carry = 0;
+        int length = Math.max(list1.getSize(), list2.getSize());
+        int currentIndex = 0;
+
+        while (currentIndex < length) {
+            if (currentIndex < list1.getSize()) {
+                addend1 = list1.get(currentIndex);
+            }
+
+            if (currentIndex < list2.getSize()) {
+                addend2 = list2.get(currentIndex);
+            }
+
+            int sum = addend1 + addend2 + carry;
+            result.add(sum % 10);
+
+            carry = sum / 10;
+            addend1 = 0;
+            addend2 = 0;
+            currentIndex++;
+        }
+
+        return result;
     }
 }
